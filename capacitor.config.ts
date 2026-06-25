9ba208ef811b7096662246e9c6a8d0e1b58b2a61
ahmedshelfa123-gif <ahmedshelfa123@gmail.com>
2026-06-25 23:46:17 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.saber.spendwise',
+  appName: 'SpendWise',
+  webDir: 'dist'
+};
+
+export default config;
